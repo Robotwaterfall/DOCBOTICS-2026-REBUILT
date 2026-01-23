0753c7705722d8be41a9b254d081c221ae41c1eb
Robotwaterfall <jaysonwelshman@gmail.com>
2026-01-23 11:07:36 -0500

    SwerveModule is finished. The next step is to make the subsystem for the Swerve Drive.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +19,83 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
    
+  }
+
+  public static class SwerveModuleConstants {
+
+    public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
+
+    public static final double kDriveMotorGearRatio = 1 / 6.75;
+    public static final double kTurningMotorGearRatio = 1 / 12.8;
+
+    public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
+    public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
+
+    public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+    public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
+
+    public static final double kTurning = 0.3; // TODO: Tune this kp for turning
+
+  }
+
+    public static final class DriveConstants {
+
+        public static final double kTrackWidth = Units.inchesToMeters(19.75); //changed
+        // Distance between right and left wheels
+        public static final double kWheelBase = Units.inchesToMeters(28.6); //changed 2025 01 25
+        // Distance between front and back wheels
+        public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+                new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+                new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, kTrackWidth / 2));
+
+      public static final int kFrontLeftDriveMotorPort = 5;
+      public static final int kBackLeftDriveMotorPort = 8;
+      public static final int kFrontRightDriveMotorPort = 7;
+      public static final int kBackRightDriveMotorPort = 3;
+
+      public static final int kFrontLeftTurningMotorPort = 10;
+      public static final int kBackLeftTurningMotorPort = 2;
+      public static final int kFrontRightTurningMotorPort = 6;
+      public static final int kBackRightTurningMotorPort = 4;
+
+      public static final boolean kFrontLeftTurningEncoderReversed = true;
+      public static final boolean kBackLeftTurningEncoderReversed = true;
+      public static final boolean kFrontRightTurningEncoderReversed = true;
+      public static final boolean kBackRightTurningEncoderReversed = true;
+
+      public static final boolean kFrontLeftDriveEncoderReversed = false;
+      public static final boolean kBackLeftDriveEncoderReversed = false;
+      public static final boolean kFrontRightDriveEncoderReversed = true;
+      public static final boolean kBackRightDriveEncoderReversed = true;
+
+      public static final int kFrontLeftDriveAbsoluteEncoderPort = 20;
+      public static final int kBackLeftDriveAbsoluteEncoderPort = 21;
+      public static final int kFrontRightDriveAbsoluteEncoderPort = 19;
+      public static final int kBackRightDriveAbsoluteEncoderPort = 22;
+
+      public static final boolean kFrontLeftDriveAbsoluteEncoderReversed = false;
+      public static final boolean kBackLeftDriveAbsoluteEncoderReversed = false;
+      public static final boolean kFrontRightDriveAbsoluteEncoderReversed = false;
+      public static final boolean kBackRightDriveAbsoluteEncoderReversed = false;
+
+      public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad = 0.3481 * 2 * Math.PI;
+      public static final double kBackLeftDriveAbsoluteEncoderOffsetRad = 0.2556 * 2 * Math.PI;
+      public static final double kFrontRightDriveAbsoluteEncoderOffsetRad = -0.3181 * 2 * Math.PI;
+      public static final double kBackRightDriveAbsoluteEncoderOffsetRad = 0.094971 * 2 * Math.PI;
+
+      public static final double kPhysicalMaxSpeedMetersPerSecond = 5;
+      public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 2 * 2 * Math.PI;
+
+      public static final double kTeleDriveMaxSpeedMetersPerSecond = kPhysicalMaxSpeedMetersPerSecond  / 1.75;
+      public static final double kTeleDriveMaxAngularSpeedRadiansPerSecond = kPhysicalMaxAngularSpeedRadiansPerSecond / 2.5;
+      public static final double kTeleDriveMaxAccelerationUnitsPerSecond = 2;
+      public static final double kTeleDriveMaxAngularAccelerationUnitsPerSecond = 2;
+
+    public static class autoTargetConstants {
+      public static final double autoOrientKp = 0.0035;
+
+    }
   }
 }
---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,134 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.SwerveModuleConstants;
+
+public class SwerveModule {
+
+    private final SparkMax driveMotor;
+    private final SparkMaxConfig driveMotorConfig = new SparkMaxConfig();
+
+    private final SparkMax turningMotor;
+    private final SparkMaxConfig turningMotorConfig = new SparkMaxConfig();
+
+    private final PIDController turningController;
+
+    private final CANcoder absoluteEncoder;
+    private final boolean absoluteEncoderReversed;
+    private final double absoluteEncoderOffsetRad;
+
+    public SwerveModule(
+        int driveMotorId, 
+    int turningMotorId, 
+        boolean driveMotorReversed,
+    boolean turningMotorReversed, 
+        int absoluteEncoderId, 
+    double absoluteEncoderOffset, 
+        boolean absoluteEncoderReversed){
+
+        this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
+        this.absoluteEncoderReversed = absoluteEncoderReversed;
+        absoluteEncoder = new CANcoder(absoluteEncoderId);
+
+        driveMotor = new SparkMax(driveMotorId, MotorType.kBrushless);
+        turningMotor = new SparkMax(turningMotorId, MotorType.kBrushless);
+
+        driveMotorConfig
+        .inverted(driveMotorReversed);
+
+        turningMotorConfig
+        .inverted(turningMotorReversed);
+
+        driveMotorConfig.encoder
+        .positionConversionFactor(SwerveModuleConstants.kDriveEncoderRot2Meter)
+        .velocityConversionFactor(SwerveModuleConstants.kDriveEncoderRPM2MeterPerSec);
+
+        turningMotorConfig.encoder
+        .positionConversionFactor(SwerveModuleConstants.kTurningEncoderRot2Rad)
+        .velocityConversionFactor(SwerveModuleConstants.kTurningEncoderRPM2RadPerSec);
+
+        turningController = new PIDController(SwerveModuleConstants.kTurning,0,0);
+
+        turningController.enableContinuousInput(-Math.PI, Math.PI);
+
+        resetEncoder();
+
+        driveMotor.configure(driveMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+        turningMotor.configure(turningMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+    }
+
+    public double getDrivePosition(){
+        return driveMotor.getEncoder().getPosition();
+    }
+
+    public double getTurningPosition(){
+        return ((absoluteEncoder.getAbsolutePosition().getValueAsDouble() * 2 * Math.PI) - absoluteEncoderOffsetRad);
+    }
+
+    public double getDriveVelocity(){
+        return driveMotor.getEncoder().getVelocity();
+    }
+
+    public double getTurningVelocity(){
+        return turningMotor.getEncoder().getVelocity();
+    }
+
+    public double getAbsoluteEncoderRad(){
+
+        double angle = (absoluteEncoder.getAbsolutePosition().getValueAsDouble()); // reads the percent of how much rotation we are reading
+        angle *= 2.0 * Math.PI; //convert to radians
+        angle -= absoluteEncoderOffsetRad;
+
+        return angle * (absoluteEncoderReversed ? -1.0 : 1.0); //gives the Encoder values on if the encoder is reversed
+    } 
+
+    public void resetEncoder(){
+        driveMotor.getEncoder().setPosition(0);
+        turningMotor.getEncoder().setPosition(getAbsoluteEncoderRad());
+    }
+
+    public SwerveModuleState getState(){
+        return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+    }
+
+    public SwerveModulePosition getSwerveModulePosition(){
+        return new SwerveModulePosition(getDrivePosition(), new Rotation2d(getTurningPosition()));
+    }
+
+    public void setDesiredState(SwerveModuleState state, boolean isDeadband){
+        if(Math.abs(state.speedMetersPerSecond) < 0.001 && isDeadband){ // were not moving do not reset the motors
+            
+            stop();
+            return;
+        }
+        
+        state = SwerveModuleState.optimize(state, getState().angle);
+        driveMotor.set(state.speedMetersPerSecond / DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+        turningMotor.set(turningController.calculate(getTurningPosition(), state.angle.getRadians()));
+        
+    }
+
+    public void stop(){
+        driveMotor.set(0);
+        turningMotor.set(0);
+        turningController.setSetpoint(0);
+    }
+
+    public void sendToDashboard(){
+        //insert live data to send dashboard here
+    }
+
+
+}
