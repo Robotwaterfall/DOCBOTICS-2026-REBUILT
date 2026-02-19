15b5bdf7a97f11b95871534cf6843329de5efca9
Robotwaterfall <jaysonwelshman@gmail.com>
2026-02-18 22:32:19 -0500

    finished clean up and comment organization.

--- src/main/java/frc/robot/subsystems/ConveyorSub.java
+++ src/main/java/frc/robot/subsystems/ConveyorSub.java
@@ -8,13 +8,11 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class ConveyorSub extends SubsystemBase {
 
-    /**Motor that allows the intake to consume coral. */
+
     private SparkMax conveyorMotor = new SparkMax(IntakeConstants.kIntakeMotorPort, MotorType.kBrushless);
-    
-    /**
-     * @return the intake motor that consumes the Coral.
-     */
-    public SparkMax getConveryorMotor() {
+  
+
+    public SparkMax getConveyorMotor() {
         return conveyorMotor;
     }
 
