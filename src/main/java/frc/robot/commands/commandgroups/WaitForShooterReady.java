f28318c9e139e57d85675e3a6826818a27be41c3
docboticsprogramming <docboticsprogramming@gmail.com>
2026-03-21 17:08:19 -0400

    Refactor FireShot and WaitForShooterReady classes to extend command functionality and improve structure. Last command group to finish is the Shooting routine.

--- src/main/java/frc/robot/commands/commandgroups/WaitForShooterReady.java
+++ src/main/java/frc/robot/commands/commandgroups/WaitForShooterReady.java
@@ -1,5 +1,22 @@
 package frc.robot.commands.commandgroups;
 
-public class WaitForShooterReady {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.AlignToHubCMD;
+import frc.robot.subsystems.ShooterSub;
+
+public class WaitForShooterReady extends Command{
+    private final ShooterSub shooterSub;
+    private final AlignToHubCMD alignToHubCMD;
+
+    public WaitForShooterReady(ShooterSub shooterSub, AlignToHubCMD alignToHubCMD){
+        this.shooterSub = shooterSub;
+        this.alignToHubCMD = alignToHubCMD;
+        addRequirements(shooterSub);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return shooterSub.isAtSetVelocityFPS() && alignToHubCMD.isFinished();
+    }
 
 }
