f11167dff4e21f64952b1f0b73bdebeb1464f8ff
docboticsprogramming <docboticsprogramming@gmail.com>
2026-03-22 21:26:03 -0400

    Delete obsolete auto routines and paths for LeftBumpSweepNeutralZone, LeftBumpSweepNeutralZone_DepotSweep, RightBoomerangStealSweep, and RightNeutralZoneDriveInStealToShootRight. Update RightBoomerangSweep with new path names and commands. Add new autos for BackUpShootCenter and BackUpShootCenter_Depot, along with new paths for CenterReverse, DepotToShootMiddle, MiddleAllianceToDepotLineUp, RightDriveIntoNeutralZone, RightNeutralZoneToShootRight, RightShootToNeutral, and RightStartToNeutralZonePrepareCollect. Modify settings to include new auto and path folders.

--- src/main/java/frc/robot/commands/AdjustHoodCMD.java
+++ src/main/java/frc/robot/commands/AdjustHoodCMD.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.HoodSub;
-import frc.robot.subsystems.ShooterSub;
 import frc.robot.subsystems.SwerveSub;
 import frc.robot.util.PoseManager;
 import frc.robot.util.ShooterLookup;
