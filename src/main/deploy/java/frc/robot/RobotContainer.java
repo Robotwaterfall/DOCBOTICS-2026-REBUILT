930228b726f7606c79cc5dfae11601d138be5bd8
docboticsprogramming <docboticsprogramming@gmail.com>
2026-03-31 15:03:01 -0400

    Cleaned Constants

--- src/main/deploy/java/frc/robot/RobotContainer.java
+++ src/main/deploy/java/frc/robot/RobotContainer.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
-  public final LedSub ledSub = new LedSub();
   public RobotContainer() {
     configureBindings();
   }
